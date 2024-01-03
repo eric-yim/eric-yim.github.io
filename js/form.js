754ab94a30bb8ea0351bfc2972e8e633be1b8ef6
Eric Yim <ericyim2007@u.northwestern.edu>
2024-01-03 15:27:30 -0800

    UP

---
+++ js/form.js
@@ -0,0 +1,38 @@
+function generateQuiz(event) {
+    const ENDPOINT = 'http://localhost:5000/api/items';
+    // Prevent the default form submission
+    event.preventDefault();
+
+    // Get the selected number of questions and content from the form
+    var selectedNumber = document.getElementById("selectNumber").value;
+    var content = document.getElementById("inputContent").value;
+    // Perform any additional logic or actions here (e.g., generate quiz based on the input)
+    console.log("Selected Number:", selectedNumber);
+    console.log("Content:", content);
+
+    var quizData = {
+        number: selectedNumber,
+        content: content
+    };
+
+    fetch(ENDPOINT, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(quizData)
+      })
+      .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+}
+
