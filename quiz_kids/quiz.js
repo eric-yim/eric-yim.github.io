b6a13bba1c82b1e6114455d01ce5ef93bf6bde68
Eric Yim <ericyim2007@u.northwestern.edu>
2024-02-24 06:16:46 -0800

    up

--- quiz_kids/quiz.js
+++ quiz_kids/quiz.js
@@ -1,10 +1,10 @@
 const ENDPOINT = 'https://h6aprpp99i.execute-api.us-west-2.amazonaws.com/prod/quizApp/';
 const NUM_PROB = 16;
 const url =ENDPOINT + 'easy-single-digit-sums';
-function getQuizSet() {
+function getQuizSet(url) {
         return new Promise(async (resolve, reject) => {
                 try {
-                  const response = await fetch(url);
+                  const response = await fetch(ENDPOINT + url);
                   const data = await response.json();
                   resolve(data);
                 } catch (error) {
