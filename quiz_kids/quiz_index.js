be8281b8fa7fe0425178e678cc71f00be1a17fb3
Eric Yim <ericyim2007@u.northwestern.edu>
2024-02-24 18:53:24 -0800

    UP

--- quiz_kids/quiz_index.js
+++ quiz_kids/quiz_index.js
@@ -4,9 +4,9 @@ function goToQuiz() {
     var quizId = document.getElementById("selectNumber").value;
     var content = document.getElementById("inputContent").value;
 
-    var targetLocation = 'https://eric-yim.github.io/quiz_kids?quizId=' + quizId + '&imageUrl=' + content;
+    var targetLocation = "https://eric-yim.github.io/quiz_kids?quizId=" + quizId + "&imageUrl=" + content;
     console.log(targetLocation);
     // Perform any additional logic or actions here (e.g., generate quiz based on the input)
-
+    window.open(targetLocation, "_blank");
     // window.location.href = targetLocation;
 }
