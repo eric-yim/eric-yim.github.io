399e9e8fb84e3381812d5c1adf5323dd05a98bc4
Eric Yim <ericyim2007@u.northwestern.edu>
2024-12-07 14:36:03 -0800

    Up

--- quiz_kids/quiz_index.js
+++ quiz_kids/quiz_index.js
@@ -3,6 +3,11 @@ function goToQuiz() {
     // Get the selected number of questions and content from the form
     var quizId = document.getElementById("selectNumber").value;
     var content = document.getElementById("inputContent").value;
+    var newImage = document.getElementById("newImage").value;
+    if (newImage.trim() != "") {
+        // Content is empty or contains only whitespace
+        console.log("Content is not empty");
+    }
 
     var targetLocation = "https://eric-yim.github.io/quiz_kids?quizId=" + quizId + "&imageUrl=" + content;
     console.log(targetLocation);
